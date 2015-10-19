b0bafebb4246daf0d9e87a56bd4238a6d69f1e69
Blake Hagen <blakehagen@gmail.com>
2015-10-19 13:06:53 -0600

    practice

---
+++ practice-gabe.js
@@ -0,0 +1,28 @@
+var Car = function(make, model, year, color){
+	this.make = make;
+	this.model = model;
+	this.year = year;
+	this.color = color;
+	this.odometer = 0;
+	this.working = true;
+}
+
+var car1 = new Car("Honda", "Odyssey", "2011", "White");
+console.log(car1);
+
+var car2 = new Car("Mitsubishi", "Montero", "2001", "Gray");
+
+var ferrari = new Car("Ferrari", "FastZ", "2016", "Red");
+
+Car.prototype = {
+	forward: function(miles){
+		this.odometer = this.odometer + miles;
+	},
+	mechanic: function(){
+		if(this.working === false){
+			this.working = true;
+		}
+	}
+};
+
+
